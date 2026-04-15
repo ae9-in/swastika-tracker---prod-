fba295bdd5aa03f33fcc0b7a4515e30af4ff4301
Jeevan Konduru <jeevankonduru2002@gmail.com>
2026-04-15 16:31:42 +0530

    Serve frontend from backend server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,27 @@
-import app from './app.js';
+import app from './src/app.js';
 import { env } from './config/env.js';
 import { pool } from './config/db.js';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
 
-const port = process.env.PORT || env.port;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const server = app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Backend listening on http://localhost:${port}`);
+const distPath = join(__dirname, '../../dist');
+
+function serveFrontend(req, res, next) {
+  const indexPath = join(distPath, 'index.html');
+  if (fs.existsSync(indexPath)) {
+    return res.sendFile(indexPath);
+  }
+  next();
+}
+
+app.use(serveFrontend);
+
+const server = app.listen(env.port, () => {
+  console.log(`Server running on port ${env.port}`);
 });
 
 function shutdown() {
