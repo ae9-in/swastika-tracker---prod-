9f2b84cb7000d23bd8f18ffc2f00260c27a2cb8f
Jeevan Konduru <jeevankonduru2002@gmail.com>
2026-04-15 16:47:39 +0530

    Test minimal handler

--- vercel_api.js
+++ vercel_api.js
@@ -1,24 +1,4 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = express();
-const distPath = path.join(__dirname, 'dist');
-
-app.use(cors());
-
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok' });
-});
-
-app.use(express.static(distPath));
-
-app.use((req, res) => {
-  res.sendFile(path.join(distPath, 'index.html'));
-});
-
-export default app;
+export default (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.send('<html><body><h1>It works!</h1></body></html>');
+};
